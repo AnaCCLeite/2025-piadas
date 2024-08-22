d18c6f9a5ba7f49e185626b8fc89391fc6d7b283
AnaCCLeite <campos.leite@aluno.ifsp.edu.br>
2024-08-21 22:20:47 -0300

    Scaffold do controller Piadas

---
+++ Models/Piada.cs
@@ -0,0 +1,14 @@
+﻿namespace Piadas.Models;
+
+public class Piada
+{
+    public int id { get; set; }
+    public string Pergunta { get; set; }
+    public string Resposta { get; set; }
+
+
+    public Piada()
+    {
+        
+    }
+}
