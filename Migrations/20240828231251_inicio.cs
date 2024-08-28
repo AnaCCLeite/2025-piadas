8ef50bb2693b3ee6f2a28c8978d7a8cda547698f
ALUNO <campos.leite@aluno.ifsp.edu.br>
2024-08-28 20:29:04 -0300

    Atualização 28.08

--- Migrations/20240828231251_inicio.cs
+++ Migrations/20240828231251_inicio.cs
@@ -5,7 +5,7 @@
 namespace Piadas.Migrations
 {
     /// <inheritdoc />
-    public partial class IníciodaAplicação : Migration
+    public partial class inicio : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
